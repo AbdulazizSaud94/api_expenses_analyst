e0aeca5e2a53fa58f172a572b28e03a9772e6fb1
Abdulaziz Saud <azoz-saud@live.com>
2019-09-24 15:26:38 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import json
 from flask import jsonify
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 
 
 app = Flask(__name__)
@@ -19,39 +19,43 @@ def index():
     df = pd.DataFrame.from_records(js)
     df = df.drop([0])
     df['amount'] = df['amount'].astype(int)
-    df["date"]= pd.to_datetime(df["date"]) 
+    # df['income'] = df['income'].astype(int)
+    df["date"] = pd.to_datetime(df["date"])
 
-
-    maxim = df['amount'].max()
+    maxim = df.amount.max()
     mini = df['amount'].min()
+    sumi = df['amount'].sum()
+    median = df['amount'].median()
     mean = df['amount'].mean()
-
-    gb = df.groupby("type")
 
     gf = df.groupby(["type"]).sum().sort_values("amount", ascending=False)
 
-    df['YearMonth'] = pd.to_datetime(df['date']).apply(lambda x: '{month}-{year}'.format(year=x.year, month=x.day))
+    perc = gf
+    perc['percentage'] = (100*perc['amount'])/(int(df.at[1, 'income']))
 
+    date_amount = df.groupby(["date"]).sum(
+    ).sort_values("amount", ascending=False)
+
+    max_amount = date_amount[date_amount.amount == date_amount.amount.max()]
+
+    df['YearMonth'] = pd.to_datetime(df['date']).apply(
+        lambda x: '{month}-{year}'.format(year=x.year, month=x.month))
     # monthly = df.groupby('YearMonth')['amount'].sum()
 
-    monthly =df.groupby(["YearMonth"]).sum().sort_values("amount", ascending=False)
+    monthly = df.groupby(["YearMonth"]).sum(
+    ).sort_values("amount", ascending=False)
 
-    print(monthly)
     print(gf)
-
-    types = []
-    typeSpend = {}
-    # for name, group in df.groupby("type"):
-    #     types.append(name)
-   
     type_js = gf.to_dict('dict')
     month_js = monthly.to_dict('dict')
-
-
+    percentage = perc.to_dict('dict')
+    # max_spend_js = max_amount.to_dict('dict')
 
     res = {
-        'maximum': f'{maxim}',
+        # 'maximum': max_spend_js,
         'minimum': f'{mini}',
+        'sum': f'{sumi}',
+        'median': f'{median}',
         'mean': f'{mean}',
         'typeBased': type_js,
         'monthBased': month_js,
@@ -61,27 +65,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-    # res = str(res, 'utf-8')
-
-    # print(res[0])
-    # f = open( 'api_flask/sheets/parsed.json', 'w')
-    # f.write(res)
-    # print(df)
-    # js = json.loads(res)
-    # # df = pd.read_json(js)
-    # print(type(js))
-    # df = pd.DataFrame.from_records(js)
-    # df = df.drop([0])
-    # print(df)
-    # maxim = df['amount'].astype(int).max()
-    # mini = df['amount'].astype(int).min()
-    # mean = df['amount'].astype(int).mean()
-    # res = json.dumps({
-    #     "maximum": f"{maxim}",
-    #     "minimum": f"{mini}",
-    #     "mean": f"{mean}",
-    # }, indent=4)
-    # print(sd[1]['amount'])
-    # print(type(request.data))
-    # print(request['data'])
